479a58ff8c236cfe8b773b538ef3fd50d53aad6e
Mucyo Papy Blaise <mucyoblaise86@gmail.com>
2026-04-08 09:19:21 +0200

    adding test  section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/NavBar";
 import HeroSection from "@/components/Herosection";
+import ArchitectureSection from "@/components/ArchitectureSection";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <HeroSection />
+      <ArchitectureSection />
     </main>
   );
 }
---
+++ components/ArchitectureSection.tsx
@@ -0,0 +1,7 @@
+export default function ArchitectureSection(){
+    return(
+        <div className="p-5">
+            This is the section of Architecture
+        </div>
+    )
+}
